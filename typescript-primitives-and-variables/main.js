37b8404e9c5ea5bcf2e2f7de6a7c281393d271a3
lindsay-jin <lindsayjin1@gmail.com>
2024-01-29 21:40:21 +0000

    completed typescript-primitives-and-variables

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const fullName = 'John';
+const isCool = true;
+const totalPets = 1;
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+const unknown = null;
+console.log('value of unknown:', unknown);
+console.log('type of unknown:', typeof unknown);
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
