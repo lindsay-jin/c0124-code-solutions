f7a2c0fc47eb8e613fc7c6b3fd0994f94a925f70
lindsay-jin <lindsayjin1@gmail.com>
2024-02-13 01:07:08 +0000

    completed dom-manipulation

---
+++ dom-manipulation/dom-manipulation-quiz-notes.md
@@ -0,0 +1,29 @@
+# dom-manipulation-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What is the `className` property of element objects?
+  it retrieves and sets the class of an element
+
+- How do you update the CSS class attribute of an element using JavaScript?
+  domElement.className = 'new class name'
+
+- What is the `textContent` property of element objects?
+  it represents the text content
+
+- How do you update the text within an element using JavaScript?
+
+domElement.textContent = 'new content'
+
+- Is the `event` parameter of an event listener callback always useful?
+  No.
+
+- Would this assignment be simpler or more complicated if we didn't use a variable to keep track of the number of clicks?
+  more complicated
+
+- Why is storing information about a program in variables better than only storing it in the DOM?
+  faster to retrieve data
+
+## Notes
---
+++ dom-manipulation/main.ts
@@ -0,0 +1,55 @@
+let numOfClicks = 0;
+const $hotButton = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+
+if (!$hotButton) throw new Error('The $hotButton query failed');
+
+$hotButton.addEventListener('click', addClicks);
+
+// function addClicks(event: Event): void {
+//   if (!$hotButton || !$clickCount)
+//     throw new Error('The $hotButton or $clickCount query failed');
+
+//   numOfClicks++;
+//   $clickCount.textContent = `clicks: ${numOfClicks}`;
+
+//   if(numOfClicks < 4){ // switch(true){case numOfClicks < 4: statement}
+//     $hotButton.className = 'hot-button cold';
+//   } else if(numOfClicks < 7){
+//     $hotButton.className = 'hot-button cool';
+//   } else if (numOfClicks < 10){
+//     $hotButton.className = 'hot-button tepid';
+//   } else if (numOfClicks < 13){
+//     $hotButton.className = 'hot-button warm';
+//   } else if (numOfClicks <16){
+//     $hotButton.className = 'hot-button hot';
+//   }else{
+//     $hotButton.className = 'hot-button nuclear';
+//   }
+// };
+
+function addClicks(): void {
+  if (!$hotButton || !$clickCount)
+    throw new Error('The $hotButton or $clickCount query failed');
+
+  numOfClicks++;
+  let temp;
+
+  if (numOfClicks < 4) {
+    // switch(true){case numOfClicks < 4: statement}
+    temp = 'cold';
+  } else if (numOfClicks < 7) {
+    temp = 'cool';
+  } else if (numOfClicks < 10) {
+    temp = 'tepid';
+  } else if (numOfClicks < 13) {
+    temp = 'warm';
+  } else if (numOfClicks < 16) {
+    temp = 'hot';
+  } else {
+    temp = 'nuclear';
+  }
+
+  $hotButton.className = `hot-button ${temp}`;
+  $clickCount.textContent = `clicks: ${numOfClicks}`;
+}
