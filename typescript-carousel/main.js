04c758045231fbc6073c352fc53d421a619a0a55
lindsay-jin <lindsayjin1@gmail.com>
2024-02-27 03:51:10 +0000

    working on dots

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -2,8 +2,9 @@
 const $img = document.querySelector('img');
 const $leftAngle = document.querySelector('.fa-angle-left');
 const $rightAngle = document.querySelector('.fa-angle-right');
+const $dots = document.querySelectorAll('.dots');
 let intervalId = setInterval(timer, 3000);
-let images = [
+const images = [
   'images/001.png',
   'images/004.png',
   'images/007.png',
@@ -11,9 +12,16 @@ let images = [
   'images/039.png',
 ];
 let index = 0;
-//automatic changing of images
+// automatic changing of images
 function timer() {
   if (index >= 0 && index < 4) {
+    $dots[index].setAttribute('class', 'fa-solid fa-circle dots');
+    for (let i = 0; i < $dots.length; i++) {
+      if (i !== index) {
+        // Update the class of other dots to just circle
+        $dots[i].setAttribute('class', 'fa-regular fa-circle dots');
+      }
+    }
     index++;
     $img?.setAttribute('src', images[index]);
   } else if (index === 4) {
@@ -21,7 +29,7 @@ function timer() {
     $img?.setAttribute('src', images[index]);
   }
 }
-//next one:
+// next one:
 $rightAngle?.addEventListener('click', next);
 function next() {
   clearInterval(intervalId);
@@ -34,7 +42,7 @@ function next() {
   }
   intervalId = setInterval(timer, 3000);
 }
-//previous one:
+// previous one:
 $leftAngle?.addEventListener('click', previous);
 function previous() {
   clearInterval(intervalId);
@@ -47,3 +55,17 @@ function previous() {
   }
   intervalId = setInterval(timer, 3000);
 }
+//<i class="fa-solid fa-circle"></i>;
+//dots:
+for (let i = 0; i < $dots.length; i++) {
+  $dots[i].addEventListener('click', () => {
+    clearInterval(intervalId);
+    for (let x = 0; x < $dots.length; x++) {
+      $dots[x].setAttribute('class', 'fa-regular fa-circle');
+    }
+    $dots[i].setAttribute('class', 'fa-solid fa-circle');
+    $img?.setAttribute('src', images[i]);
+  });
+}
+//i want the accompanying dot to be solid when the image shows up
+// i want the timer to reset after i click on dot
--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -1,6 +1,7 @@
 const $img = document.querySelector('img');
 const $leftAngle = document.querySelector('.fa-angle-left');
 const $rightAngle = document.querySelector('.fa-angle-right');
+const $dots = document.querySelectorAll('.dots');
 
 let intervalId = setInterval(timer, 3000);
 const images = [
@@ -15,6 +16,13 @@ let index = 0;
 // automatic changing of images
 function timer(): void {
   if (index >= 0 && index < 4) {
+    $dots[index].setAttribute('class', 'fa-solid fa-circle dots');
+    for (let i = 0; i < $dots.length; i++) {
+      if (i !== index) {
+        // Update the class of other dots to just circle
+        $dots[i].setAttribute('class', 'fa-regular fa-circle dots');
+      }
+    }
     index++;
     $img?.setAttribute('src', images[index]);
   } else if (index === 4) {
@@ -54,3 +62,20 @@ function previous(): void {
 
   intervalId = setInterval(timer, 3000);
 }
+
+// <i class="fa-solid fa-circle"></i>;
+
+// dots:
+for (let i = 0; i < $dots.length; i++) {
+  $dots[i].addEventListener('click', () => {
+    clearInterval(intervalId);
+    for (let x = 0; x < $dots.length; x++) {
+      $dots[x].setAttribute('class', 'fa-regular fa-circle');
+    }
+    $dots[i].setAttribute('class', 'fa-solid fa-circle');
+    $img?.setAttribute('src', images[i]);
+  });
+}
+
+// i want the accompanying dot to be solid when the image shows up
+// i want the timer to reset after i click on dot
