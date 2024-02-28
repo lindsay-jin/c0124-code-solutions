0ce0e76ae6fcf1f6192e2c32b672d3ef9644b154
lindsay-jin <lindsayjin1@gmail.com>
2024-02-28 02:26:21 +0000

    made edits to interval.ts

--- typescript-timers/interval.ts
+++ typescript-timers/interval.ts
@@ -1,18 +1,15 @@
 const $countdown = document.querySelector('.countdown-display');
 
 const intervalId = setInterval(timer, 1000);
-setInterval(timer, 1000);
 
 let count = 4;
 function timer(): void {
-  if (!$countdown) throw new Error('The $h1 query has failed.');
-  if ($countdown.textContent === '4') {
-    $countdown.textContent = (count--).toString();
-  } else if ($countdown.textContent === '3') {
-    $countdown.textContent = (count--).toString();
-  } else if ($countdown.textContent === '2') {
-    $countdown.textContent = (count--).toString();
-  } else if ($countdown.textContent === '1')
+  count--;
+  if (!$countdown) throw new Error('The $countdown query failed.');
+  if (count > 0) {
+    $countdown.textContent = count.toString();
+  } else {
+    clearInterval(intervalId);
     $countdown.textContent = '~Earth Beeeelooowww Us~';
-  clearInterval(intervalId);
+  }
 }
