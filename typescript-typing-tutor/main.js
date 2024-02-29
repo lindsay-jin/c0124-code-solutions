6778f6f30b3677dbb70901017a6b41a171fea77d
lindsay-jin <lindsayjin1@gmail.com>
2024-02-29 02:24:08 +0000

    working on typing accuracy function

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -5,33 +5,31 @@ const $characters = document.querySelectorAll('span');
 let $currentCharacter = $characters[currentIndex];
 if (!$characters) throw new Error('The $characters query failed.');
 if (!$currentCharacter) throw new Error('The $currentCharacter query failed.');
+//let greenCharacter = 0;
 document.addEventListener('keydown', (event) => {
-  const typedKey = event.key; //whatever I typed
-  // let correctCount = 0;
+  const typedKey = event.key; // whatever I typed
   if (typedKey === $currentCharacter.textContent) {
     $currentCharacter.classList.add('green');
     $currentCharacter.classList.remove('underline');
-    // correctCount++
     currentIndex++;
     $currentCharacter = $characters[currentIndex];
     $currentCharacter.classList.add('underline');
   } else {
     $currentCharacter.classList.add('red');
-  } // return correctCount;
+  }
 });
-// show typing accuracy
-// const $openModal = document.querySelector('.open-modal'); // open dialog button
-// const $dismissModal = document.querySelector('.dismiss-modal');
-// const $dialog = document.querySelector('dialog');
-// if (!$openModal) throw new Error('The $openModal query has failed.');
-// if (!$dismissModal) throw new Error('The $dismissModal query has failed.');
-// if (!$dialog) throw new Error('The $dialog query has failed.');
-// function open(): void {
-//   $dialog?.showModal();
-// }
-// $openModal.addEventListener('click', open);
-// function close(): void {
-//   $dialog?.close();
-// }
-// $dismissModal.addEventListener('click', close);
-// When the user finishes typing the phrase, they can be asked if they'd like to play again.
+const $openModal = document.querySelector('.open-modal'); // open dialog button
+const $dismissModal = document.querySelector('.dismiss-modal');
+const $dialog = document.querySelector('dialog');
+if (!$openModal) throw new Error('The $openModal query has failed.');
+if (!$dismissModal) throw new Error('The $dismissModal query has failed.');
+if (!$dialog) throw new Error('The $dialog query has failed.');
+function open() {
+  $dialog?.showModal();
+}
+$openModal.addEventListener('click', open);
+function close() {
+  $dialog?.close();
+}
+$dismissModal.addEventListener('click', close);
+//track how many right you got: make a variable correctCount, everytime, it turns green, update
--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -7,41 +7,41 @@ let $currentCharacter = $characters[currentIndex];
 if (!$characters) throw new Error('The $characters query failed.');
 if (!$currentCharacter) throw new Error('The $currentCharacter query failed.');
 
+// let greenCharacter = 0;
+
 document.addEventListener('keydown', (event) => {
   const typedKey = event.key; // whatever I typed
-  // let correctCount = 0;
 
   if (typedKey === $currentCharacter.textContent) {
     $currentCharacter.classList.add('green');
+
     $currentCharacter.classList.remove('underline');
-    // correctCount++
     currentIndex++;
     $currentCharacter = $characters[currentIndex];
     $currentCharacter.classList.add('underline');
   } else {
     $currentCharacter.classList.add('red');
-  } // return correctCount;
+  }
 });
 
-// show typing accuracy
-// const $openModal = document.querySelector('.open-modal'); // open dialog button
-// const $dismissModal = document.querySelector('.dismiss-modal');
-// const $dialog = document.querySelector('dialog');
+const $openModal = document.querySelector('.open-modal'); // open dialog button
+const $dismissModal = document.querySelector('.dismiss-modal');
+const $dialog = document.querySelector('dialog');
 
-// if (!$openModal) throw new Error('The $openModal query has failed.');
-// if (!$dismissModal) throw new Error('The $dismissModal query has failed.');
-// if (!$dialog) throw new Error('The $dialog query has failed.');
+if (!$openModal) throw new Error('The $openModal query has failed.');
+if (!$dismissModal) throw new Error('The $dismissModal query has failed.');
+if (!$dialog) throw new Error('The $dialog query has failed.');
 
-// function open(): void {
-//   $dialog?.showModal();
-// }
+function open(): void {
+  $dialog?.showModal();
+}
 
-// $openModal.addEventListener('click', open);
+$openModal.addEventListener('click', open);
 
-// function close(): void {
-//   $dialog?.close();
-// }
+function close(): void {
+  $dialog?.close();
+}
 
-// $dismissModal.addEventListener('click', close);
+$dismissModal.addEventListener('click', close);
 
-// When the user finishes typing the phrase, they can be asked if they'd like to play again.
+// track how many right you got: make a variable correctCount, everytime, it turns green, update
